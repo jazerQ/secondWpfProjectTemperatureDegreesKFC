bc1d27313d46d3fd21a9b7d63f1f26bea3e4664c
Your Name <you@example.com>
2024-09-21 11:57:41 +0300

    main commit

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temperatureDegrees
+{
+    internal static class KelvinFarenheitCelcium
+    {
+        public static void Comparison(string firstBox, string secondBox, ref float degrees) {
+            if (firstBox == secondBox)
+            {
+                return;
+            }
+            else if (firstBox == "°K" && secondBox == "°F")
+            {
+                degrees = (degrees + 459) / 1.8f;
+                degrees = (float)Math.Round(degrees, 1);
+            }
+            else if (firstBox == "°K" && secondBox == "°C")
+            {
+                degrees -= 273;
+            }
+            else if (firstBox == "°C" && secondBox == "°K")
+            {
+                degrees += 273;
+            }
+            else if (firstBox == "°C" && secondBox == "°F")
+            {
+                degrees = (float)Math.Round((degrees * 1.8f) + 32, 1);
+            }
+            else if (firstBox == "°F" && secondBox == "°K") {
+                degrees = (float)Math.Round((degrees + 459) / 1.8f, 1);
+            }
+            else if( firstBox == "°F" && secondBox == "°C")
+            {
+                degrees = (float)Math.Round((degrees - 32) / 1.8f, 1);
+            }
+        }
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace temperatureDegrees
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btn.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string firstBox = CBox1.SelectedItem.ToString().Split(' ')[1];
+            string secondBox = CBox2.SelectedItem.ToString().Split(' ')[1];
+            float.TryParse(TBox.Text, out float degrees);
+            KelvinFarenheitCelcium.Comparison(firstBox, secondBox, ref degrees);
+            WasHiddenLabel.Opacity = 1;
+            Result.Content = $"{degrees.ToString()}{secondBox}";
+        }
+    }
+}
